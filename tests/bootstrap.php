9e777f5b921de6ff69b74459af5678bf06bb80b7
Dominik Zogg <dominik.zogg@gmail.com>
2015-09-12 14:35:49 +0200

    psr-4

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,6 @@
-<?php
 
+<?php
 $loader = require __DIR__.'/../vendor/autoload.php';
-$loader->add('Saxulum\Tests', __DIR__);
+$loader->setPsr4('Saxulum\Tests\DoctrineMongoDbOdm\\', __DIR__);
 
 \Doctrine\Common\Annotations\AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
